bbacd87e32f5c5a7a3c230d9bc8e9bff4119fb51
Maxence <levesqu221@campus.unicaen.fr>
2023-10-23 15:06:15 +0200

    fix: Manager.php

---
+++ Modele/Manager.php
@@ -0,0 +1,29 @@
+<?php
+
+class Manager {
+
+    protected function con(){
+        $envPath=__DIR__ .'.env';
+        if (file_exists($envPath)) {
+            $env = parse_ini_file($envPath);
+            foreach ($env as $key => $value) {
+                $_ENV[$key] = $value;
+                putenv("$key=$value");
+            }
+        }
+        
+        $dbHost = $_ENV['DB_HOST'];
+        $dbName = $_ENV['DB_NAME'];
+        $dbUser = $_ENV['DB_USER'];
+        $dbPassword = $_ENV['DB_PASSWORD'];
+        
+        try {
+            $conn = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPassword);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
+        } catch (PDOException $e) {
+            echo "Erreur : " . $e->getMessage();
+        }
+    }
+}
+?>
---
+++ Modele/RecipesManager.php
@@ -0,0 +1,11 @@
+<?php
+
+class RecipesManager extends Manager{
+
+   public function getRecipes(){
+        $db = $this->con();
+   }
+
+}
+
+?>
