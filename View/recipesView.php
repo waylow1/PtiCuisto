6d9929984370c8a5de2216c29199e12ef2fc664f
Noé Tourbillon <noe.tourbillon@etu.unicaen.fr>
2023-10-31 10:48:48 +0100

    fix: test

--- View/recipesView.php
+++ View/recipesView.php
@@ -9,77 +9,7 @@
     <?php require_once 'PtitCuistot/Controlle/RecipesController.php';
     $control = new RecipesController();
     $result = $control->getAllRecipes();
-    echo 'getAllRecipes() : <br>';
-    for($i = 0; $i< count($result);$i++){
-        echo 'Titre: ', $result[$i]['RE_TITLE']  ;
-        echo '<br>';
-        echo 'Catégorie: ', $result[$i]['CA_TITLE'];
-        echo '<br>';
-        echo 'Recette: ', $result[$i]['RE_CONTENT'];
-        echo '<br>';
-        echo 'Utilisateur ', $result[$i]['US_PSEUDO'];
-        echo '<br> <br>';
-    }
-
-    $result = $control->getRecipe(2);
-    echo 'GetRecipe() : <br>';
-    echo 'Titre: ', $result[0]['RE_TITLE'];
-    echo '<br>';
-    echo 'Catégorie: ', $result[0]['CA_TITLE'];
-    echo '<br>';
-    echo 'Recette: ', $result[0]['RE_CONTENT'];
-    echo '<br>';
-    echo 'Utilisateur ', $result[0]['US_PSEUDO'];
-    echo '<br> <br>';
-
-
-    $result = $control->getRecipesOfUser(1);
-    echo 'getRecipesOfUser() : <br>';
-    for($i = 0; $i< count($result);$i++){
-        echo 'Titre: ', $result[$i]['RE_TITLE']  ;
-        echo '<br>';
-        echo 'Catégorie: ', $result[$i]['CA_TITLE'];
-        echo '<br>';
-        echo 'Recette: ', $result[$i]['RE_CONTENT'];
-        echo '<br>';
-        echo 'Utilisateur ', $result[$i]['US_PSEUDO'];
-        echo '<br> <br>';
-    }
-
-    
-    echo 'getRecipesByIngredient() : <br>';
-    $result = $control->getRecipesByIngredient('Ingredient 1');
-    echo 'Titre: ', $result[0]['RE_TITLE']  ;
-    echo '<br>';
-    echo 'Catégorie: ', $result[0]['CA_TITLE'];
-    echo '<br>';
-    echo 'Recette: ', $result[0]['RE_CONTENT'];
-    echo '<br>';
-    echo 'Utilisateur ', $result[0]['US_PSEUDO'];
-    echo '<br> <br>';
-
-
-    echo 'getRecipesByTags() : <br>';
-    $result = $control->getRecipesByTag('Tag 1');
-    echo 'Titre: ', $result[0]['RE_TITLE']  ;
-    echo '<br>';
-    echo 'Catégorie: ', $result[0]['CA_TITLE'];
-    echo '<br>';
-    echo 'Recette: ', $result[0]['RE_CONTENT'];
-    echo '<br>';
-    echo 'Utilisateur ', $result[0]['US_PSEUDO'];
-    echo '<br> <br>';
-    
-    echo 'getLatestRecipe() : <br>';
-    $result = $control->getLatestRecipe();
-    echo 'Titre: ', $result[0]['RE_TITLE']  ;
-    echo '<br>';
-    echo 'Catégorie: ', $result[0]['CA_TITLE'];
-    echo '<br>';
-    echo 'Recette: ', $result[0]['RE_CONTENT'];
-    echo '<br>';
-    echo 'Utilisateur ', $result[0]['US_PSEUDO'];
-    echo '<br> <br>';
+   
     ?>
 
 
