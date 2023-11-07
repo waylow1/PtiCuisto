db2534c18a084b2645ea91771e080ea8091fab1b
martin <22201275@etu.unicaen.fr>
2023-11-07 08:58:43 +0100

    Build: getCurrentUser query

--- Modele/UsersManager.php
+++ Modele/UsersManager.php
@@ -27,7 +27,23 @@ class UsersManager extends Manager{
         session_destroy($_SESSION['username'],$_SESSION['password'],$_SESSION['type']);
     }
 
+    public function getRecipesOfUser($us_id){
+        $connexion = $this->con();
+        $recipe = $connexion->query("SELECT RE_ID, US_ID,CA_ID,RE_TITLE,RE_CONTENT,RE_SUMMARY,RE_IMAGE,CA_TITLE,US_PSEUDO 
+        from RECIPE 
+        join CATEGORY using(CA_ID) 
+        join USERS using(US_ID)
+        where US_ID='$us_id'");
+        $res = $recipe->fetchAll(PDO::FETCH_ASSOC);
+        return $res;
+     }
 
-
-
+    public function getCurrentUserID() {
+        $connexion = $this->con();
+        $user = $connexion->prepare('SELECT US_ID from USERS where US_PSEUDO like :name');
+        $user->bindParam('name', $_POST['userName']);
+        $user->execute();
+        $userID = $user->fetchall();
+        return $userID;
+    }
 }
