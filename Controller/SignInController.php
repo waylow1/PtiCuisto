0139dcd63af4ec0b00eafad20d044f5eab523203
Your Name <you@example.com>
2023-11-09 15:22:31 +0100

    add hash password and verification password

--- Controller/SignInController.php
+++ Controller/SignInController.php
@@ -22,15 +22,18 @@ class SignInController extends Controller
         $temp = $this->manager->selectMaxID();
         $var = $temp[0]['max'] +1;
         if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['pseudo'], $_POST['password1'], $_POST['password2'], $_POST['mail'],$_POST['firstname'], $_POST['lastname']))  {
-            if ($_POST['password1']==$_POST['password2']){
-                $insert= $this->manager->insertUser($var, $_POST['pseudo'], $_POST['password2'], $_POST['mail'],$_POST['firstname'], $_POST['lastname']);
-                if(isset($insert)){
-                    $_GET['action']='Profile';
-                    header('Location :'.$_SESSION['dir']);
+            if($_POST['password1'] == $_POST['password2']){
+                if ($_POST['password1']==$_POST['password2']){
+                    $insert= $this->manager->insertUser($var, $_POST['pseudo'], password_hash($_POST['password2'], PASSWORD_DEFAULT), $_POST['mail'],$_POST['firstname'], $_POST['lastname']);
+                    if(isset($insert)){
+                        $_GET['action']='Profile';
+                        header('Location :'.$_SESSION['dir']);
+                    }
                 }
             }
             else{
-                echo "mdp pas indentique";
+                echo '<script> alert ("mot de passe incorrect") </script>';
+                include $_SESSION['dir']. '/View/SignInView.php';
             }
         }
         include $_SESSION['dir'] . '/View/SignInView.php'; 
