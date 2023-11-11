57ebe9678f35f6968a157bca5aece74643d51343
waylow <max2742contact@gmail.com>
2023-11-11 23:48:34 +0100

    fix: trying to fix

--- Controller/CreateRecipeController.php
+++ Controller/CreateRecipeController.php
@@ -1,5 +1,4 @@
 <?php
-
 require_once $_SESSION['dir'] . '/Controller/Controller.php';
 require_once $_SESSION['dir'] . '/Modele/RecipesManager.php';
 
@@ -12,38 +11,33 @@ class CreateRecipeController extends Controller
 
     public function run()
     {
-        
         $allIngredient = $this->manager->getAllIngredients();
         if (isset($_GET['Ingredient'])) {
             $ingredientName = $_GET['Ingredient'];
             $count = $this->manager->getIngredient($ingredientName);
             if ($count == 0) {
-                echo "<script> console.log('la');</script>";
                 $this->manager->insertIngredient($ingredientName);
-                $_SESSION['ingredient'][] = $ingredientName;
-                echo '<script>console.log(' . json_encode($_SESSION['ingredient']) . ')</script>;';
+                
             } else {
-                echo "<script> console.log('ici');</script>";
-                $_SESSION['ingredient'][] = $ingredientName;
-                echo '<script>console.log(' . json_encode($_SESSION['ingredient']) . ');</script>';
+                
             }
         }
+
         if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submitRecipe'])) {
-            echo "<script> console.log('ici');</script>";
+
             $uploadDirectory = $_SESSION['dir'] . '/assets/dish/';
             $file = $_FILES['recipePicture'];
             $fileName = $file['name'];
             $tmpFilePath = $file['tmp_name'];
             $destination = $uploadDirectory . $fileName;
+
             $recipeId = $this->manager->createRecipe($fileName);
 
-            if (isset($_SESSION['ingredient'])) {
-                foreach ($_SESSION['ingredient'] as $ingredient) {
+            if (!empty($ingredientArray)) {
+                foreach ($ingredientArray as $ingredient) {
                     $this->manager->insertIngredientInRecipe($ingredient, $recipeId);
                 } 
             }
-            
-
             if (move_uploaded_file($tmpFilePath, $destination)) {
                 echo "<script> alert('Recette téléversée'); </script>";
                 $_GET['action'] = "";
